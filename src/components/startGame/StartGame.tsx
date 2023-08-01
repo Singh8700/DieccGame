7eb7f2c6d8c44268cfd81118adb45c4fb3fa5157
Singh8700 <rohit.si8700@gmail.com>
2023-08-01 14:47:38 +0530

    first-commit

--- src/components/startGame/StartGame.tsx
+++ src/components/startGame/StartGame.tsx
@@ -5,7 +5,7 @@ const StartGame = ({ toggle }) => {
   return (
     <Container className="flex flex-wrap">
       <div className="imgSection">
-        <img src="/images/dices.png" />
+        <img src="./images/dices.png" />
       </div>
       <div className="content">
         <h1>Dice Game</h1>
